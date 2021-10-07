0f2dfeef839ddeed75177b77d0c15dc15c67d2cb
zizy97 <supuntharuka9749@gmail.com>
2021-10-07 15:52:47 +0530

    test styled component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import styled from "styled-components";
+
+const Title = styled.h1`
+  font-size:3em;
+  text-align:center;
+  color:palevioletred
+`;
+
+const Wrapper = styled.section`
+  padding:4em;
+  background:papayawhip;
+`;
 
 function App() {
   return (
-    <h1 style={{textAlign:"center"}}>LMS</h1>
+    <Wrapper>
+      <Title style={{textAlign:"center"}}>LMS</Title>
+    </Wrapper>
+    
   );
 }
 
